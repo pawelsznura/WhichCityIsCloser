3558031fedf72761f5bd13d7fad9d8458f377e12
Pawel <p.sznura@rgu.ac.uk>
2021-10-18 20:52:19 +0100

    high score and option fragment

--- app/src/main/java/com/pawelsznuradev/whichcityiscloser/OptionsFragment.java
+++ app/src/main/java/com/pawelsznuradev/whichcityiscloser/OptionsFragment.java
@@ -2,18 +2,25 @@ package com.pawelsznuradev.whichcityiscloser;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link OptionsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OptionsFragment extends Fragment {
+public class OptionsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +65,52 @@ public class OptionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_options, container, false);
+
+        // Spinner for units selection Metric or Imperial
+        Spinner spinner = (Spinner) view.findViewById(R.id.units_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.units_array, R.layout.support_simple_spinner_dropdown_item);
+
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+        // TODO dark mode on, switch off bug
+        // Switch for dark mode
+        // base on https://stackoverflow.com/questions/11278507/android-widget-switch-on-off-event-listener
+        Switch darkModeSwitch = (Switch) view.findViewById(R.id.darkModeSwitch);
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.i("Dark mode = ", String.valueOf(b));
+                if (b) { // dark mode ON
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else { // dark mode OFF
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+                }
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_options, container, false);
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        // Spinner Units metric = 0, imperial = 1
+        if (i == 0) {
+            Log.i("Unit selected", "Metric");
+        } else if (i == 1) {
+            Log.i("Unit selected", "Imperial");
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
