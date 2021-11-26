873117c1598dfad2fbf12e904e5e71aa5d517117
Pawel <p.sznura@rgu.ac.uk>
2021-11-26 14:52:59 +0000

    new vars in City class

--- app/src/main/java/com/pawelsznuradev/whichcityiscloser/City.java
+++ app/src/main/java/com/pawelsznuradev/whichcityiscloser/City.java
@@ -10,21 +10,50 @@ import org.json.JSONObject;
  */
 
 public class City {
+    /**
+     * Example:
+     * "id":45633
+     * "wikiDataId":"Q84"
+     * "type":"CITY"
+     * "city":"London"
+     * "name":"London"
+     * "country":"United Kingdom"
+     * "countryCode":"GB"
+     * "region":"England"
+     * "regionCode":"ENG"
+     * "latitude":51.507222222
+     * "longitude":-0.1275
+     * "population":8908081
+     */
 
     private int id;
     private String wikiDataId;
+    private String type;
+    private String city;
     private String name;
+    private String country;
+    private String countryCode;
+    private String region;
+    private String regionCode;
     private double latitude;
     private double longitude;
+    private int population;
 
 
     public City(JSONObject response) {
         try {
             id = response.getInt("id");
             wikiDataId = response.getString("wikiDataId");
+            type = response.getString("type");
+            city = response.getString("city");
             name = response.getString("name");
+            country = response.getString("country");
+            countryCode = response.getString("countryCode");
+            region = response.getString("region");
+            regionCode = response.getString("regionCode");
             latitude = response.getDouble("latitude");
             longitude = response.getDouble("longitude");
+            population = response.getInt("population");
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -89,5 +118,61 @@ public class City {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getRegionCode() {
+        return regionCode;
+    }
+
+    public void setRegionCode(String regionCode) {
+        this.regionCode = regionCode;
     }
 }
